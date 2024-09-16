8b23b86c30eb55adb1233c4b257d9186707fc497
Gadeschii <javier.izquierdo.102.ulpgc@gmail.com>
2024-09-16 16:47:01 +0100

    Add Meadsure Voltage, Add thread processor, Restructure code

--- src/main/main.ino
+++ src/main/main.ino
@@ -7,8 +7,8 @@
 #include <ArduinoOTA.h>
 
 // WiFi credentials
-#define WIFI_SSID "WorkinCholas"
-#define WIFI_PASS "WiC!2023"
+#define WIFI_SSID "UPE"
+#define WIFI_PASS "redwifi.upe"
 
 // Adafruit IO configuration
 #define AIO_USERNAME  "Edudemos"
@@ -16,9 +16,10 @@
 AdafruitIO_WiFi io(AIO_USERNAME, AIO_KEY, WIFI_SSID, WIFI_PASS);
 AdafruitIO_Feed *temperature = io.feed("temperature");
 AdafruitIO_Feed *humidity = io.feed("humidity");
-AdafruitIO_Feed *ldrFeed = io.feed("ldr"); 
+AdafruitIO_Feed *ldrFeed = io.feed("ldr");
+AdafruitIO_Feed *microvoltsFeed = io.feed("microvolts"); // New feed for microvolts
 
-// DHT Sensor setup
+//DHT Sensor setup
 #define DHTPIN 32
 #define DHTTYPE DHT11
 DHT dht(DHTPIN, DHTTYPE);
@@ -35,31 +36,153 @@ const int servoPin = 33;
 const int OFFSET = 75;
 Servo myServo;
 
-// Interval for sensor readings (5 minutes in milliseconds)
-const unsigned long SENSOR_INTERVAL = 5 * 60 * 1000;
-unsigned long lastSensorReadTime = 0;
+// Analog pin and resistors setup
+const int analogPin = 39; 
+const float R1 = 1000.0; // 1kΩ resistor
+const float R2 = 1000.0; // 1kΩ resistor
+
+// Interval for sensor readings
+const unsigned long SENSOR_INTERVAL_5_MIN = 5 * 60 * 1000; // 5 minutes
+const unsigned long SENSOR_INTERVAL_10_SEC = 10 * 1000; // 10 seconds
+
+// Variable to store the last microvolts value
+float lastMicrovolts = -1;
+
+// Variables to store the last sensor read times
+unsigned long lastSensorReadTime5Min = 0;
+unsigned long lastSensorReadTime10Sec = 0;
+
+// Function declarations
+void setupWiFi();
+void setupOTA();
+void setupDHT();
+void setupLEDsAndServo();
+void handleOTA();
+void updateSensorData();
+void updateLDRAndServo();
+void readVoltage();
+void readSensorTask1(void * parameter);
+void readSensorTask2(void * parameter);
+void readVoltageTask(void * parameter);
 
 void setup() {
   Serial.begin(115200); // Start serial communication at 115200 baud rate
+  analogReadResolution(12); // ESP32 has a 12-bit ADC
+  analogSetAttenuation(ADC_11db); // Set attenuation for higher sensitivity
 
   setupWiFi(); // Setup WiFi connection
   setupOTA(); // Setup OTA for remote updates
   setupDHT(); // Setup DHT sensor
   setupLEDsAndServo(); // Setup LED pins and servo
+
+  // Create tasks
+  xTaskCreatePinnedToCore(
+    readSensorTask1,   // Task function
+    "ReadSensorTask1", // Name of the task
+    10000,             // Stack size
+    NULL,              // Task input parameter
+    1,                 // Priority of the task
+    NULL,              // Task handle
+    0                  // Core where the task should run
+  );
+
+  xTaskCreatePinnedToCore(
+    readSensorTask2,   // Task function
+    "ReadSensorTask2", // Name of the task
+    10000,             // Stack size
+    NULL,              // Task input parameter
+    1,                 // Priority of the task
+    NULL,              // Task handle
+    1                  // Core where the task should run
+  );
+
+  xTaskCreatePinnedToCore(
+    readVoltageTask,   // Task function
+    "ReadVoltageTask", // Name of the task
+    10000,             // Stack size
+    NULL,              // Task input parameter
+    1,                 // Priority of the task
+    NULL,              // Task handle
+    1                  // Core where the task should run
+  );
 }
 
 void loop() {
   handleOTA(); // Handle OTA updates
+  io.run(); // Maintain connection to Adafruit IO
+}
 
-  unsigned long currentMillis = millis();
-  if (currentMillis - lastSensorReadTime >= SENSOR_INTERVAL) {
-    lastSensorReadTime = currentMillis;
-    updateSensorData(); // Read and process sensor data
+void readSensorTask1(void * parameter) {
+  for (;;) {
+    Serial.println("----------------------- Measured Temperature / Humidity -----------------------  ");
+    updateSensorData(); // Read and process sensor data 
+    vTaskDelay(SENSOR_INTERVAL_5_MIN / portTICK_PERIOD_MS); // Delay for 5 minutes
+  }
+}
+
+void readSensorTask2(void * parameter) {
+  for (;;) {
+    Serial.println("----------------------------- Measured LDR Motor -----------------------------  ");
     updateLDRAndServo(); // Read LDR values and control servo
+    vTaskDelay(SENSOR_INTERVAL_5_MIN / portTICK_PERIOD_MS); // Delay for 5 minutes
+  }
+}
+
+void readVoltageTask(void * parameter) {
+  for (;;) {
+    Serial.println("----------------------------- Measured DC Motor (V) ----------------------------  ");
+    readVoltage(); // Read and process voltage
+    vTaskDelay(SENSOR_INTERVAL_10_SEC / portTICK_PERIOD_MS); // Delay for 10 Seg
+  }
+}
+void readVoltage() {
+  
+  // Take multiple readings to reduce noise
+  const int numReadings = 1000;
+  unsigned long sum = 0;
+
+  for (int i = 0; i < numReadings; i++) {
+    int reading = analogRead(analogPin);
+    sum += reading;
   }
 
-  delay(1000); // Wait one second before the next loop iteration
+  float averageReading = sum / (float)numReadings;
+  Serial.print("Average ADC reading: ");
+  Serial.println(averageReading);  // Display the average value
+
+  // Calculate voltage
+  float voltage = (averageReading / 4095.0) * 3.3;  // For 12-bit ADC (0-4095)
+  Serial.print("Measured voltage (V): ");
+  Serial.println(voltage);  // Display the measured voltage
+
+  // Adjust for voltage divider
+  float inputVoltage = voltage * 2;
+
+  // Convert to microvolts
+  float microvolts = inputVoltage * 1000000.0;
+  Serial.print("Input voltage (µV): ");
+  Serial.println(microvolts);
+
+  // Apply a minimum threshold to filter noise
+  const float threshold = 10.0;  // Threshold in microvolts
+  if (microvolts < threshold) {
+    microvolts = 0;
+  }
+
+  // Check if the microvolts value has changed
+  if (microvolts != lastMicrovolts) {
+    Serial.print("Measured voltage: ");
+    Serial.print(microvolts, 2);  // Display with 2 decimal places
+    Serial.println(" µV");
+
+    // Send microvolts to Adafruit IO
+    microvoltsFeed->save(microvolts);
+
+    // Update the last microvolts value
+    lastMicrovolts = microvolts;
+  }
 }
+
 
 void setupWiFi() {
   // Connect to the WiFi network
@@ -70,11 +193,9 @@ void setupWiFi() {
     Serial.print(".");
   }
   Serial.println("Connected to WiFi");
-
   // Connect to Adafruit IO
   Serial.print("Connecting to Adafruit IO");
   io.connect();
-
   // Wait for the connection to be established
   while(io.status() < AIO_CONNECTED) {
     Serial.print(".");
@@ -88,7 +209,6 @@ void setupOTA() {
   // Initialize ArduinoOTA for over-the-air updates
   ArduinoOTA.setHostname("ESP32-OTA");
   // ArduinoOTA.setPassword("admin"); // Uncomment to set a password for OTA updates
-
   // Define OTA event handlers
   ArduinoOTA.onStart([]() {
     Serial.println("Starting OTA update...");
@@ -99,7 +219,6 @@ void setupOTA() {
   ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
     Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
   });
-
   ArduinoOTA.begin(); // Start the OTA service
 }
 
@@ -119,8 +238,6 @@ void setupLEDsAndServo() {
 }
 
 void updateSensorData() {
-  io.run(); // Maintain connection to Adafruit IO
-
   // Read values from DHT sensor
   float h = dht.readHumidity();
   float t = dht.readTemperature();
@@ -169,7 +286,7 @@ void updateLDRAndServo() {
 
   // Move the servo to the calculated angle
   myServo.write(angle);
-  int sum1, sum2 = 0; 
+  int sum1 = 0, sum2 = 0; 
   const int numReadings = 10; // Number of readings to calculate the average
 
   for (int i = 0; i < numReadings; i++) {
@@ -177,7 +294,7 @@ void updateLDRAndServo() {
     sum2 += analogRead(ldrPin2); // Sum the values read from the LDR
     delay(50); // Small pause between readings
   }
-  long sum = (sum1+sum2)/2;
+  long sum = (sum1 + sum2) / 2;
   int average = sum / numReadings; // Calculate the average value
 
   // Send the average value to Adafruit IO
@@ -188,9 +305,9 @@ void updateLDRAndServo() {
 
   // Print LDR values and servo angle
   Serial.print("LDR1 Value: ");
-  Serial.print(valueLdrPin1);
-  Serial.print(", LDR2 Value: ");
-  Serial.print(valueLdrPin2);
-  Serial.print(", Servo Angle: ");
+  Serial.println(valueLdrPin1);
+  Serial.print("LDR2 Value: ");
+  Serial.println(valueLdrPin2);
+  Serial.print("Servo Angle: ");
   Serial.println(angle);
 }
